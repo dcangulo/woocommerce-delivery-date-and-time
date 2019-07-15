895b741989273cfede40105d8338c65476c9b9dd
David Angulo <hello@davidangulo.xyz>
2019-07-15 13:02:14 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: WooCommerce Delivery Date and Time
+  Plugin URI: https://github.com/dcangulo/woocommerce-delivery-date-and-time
+  Description: A WordPress plugin for Estong's Bellychon booking
+  Version: 1.0.0
+  Author: David Angulo
+  Author URI: https://www.davidangulo.xyz/
+*/
